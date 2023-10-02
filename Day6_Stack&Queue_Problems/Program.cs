f0b78d7515ac5e459aa22f1dfb83323a50217ae7
Rohan <rohandute1@gmail.com>
2023-10-02 15:26:19 +0530

    [rohan] Initial welcome message

---
+++ Day6_Stack&Queue_Problems/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Stack_Queue_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue problems");
+            Console.ReadLine();
+        }
+    }
+}
